9ee311987d6d463a2abc78f4a216d4999319d8ac
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-04-21 17:14:34 -0700

    added favicon, and fields

---
+++ main.js
@@ -0,0 +1,9 @@
+function clearResults(){
+   var text_field = document.getElementById('search-input');
+   text_field.value ="";
+   var select_field1 = document.getElementById('default1');
+   select_field1.selected = "true";
+   var select_field2 = document.getElementById('default2');
+   select_field2.selected = "true";
+
+}
